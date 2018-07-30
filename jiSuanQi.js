a52713b461b9e13f7f846ee121840415135ed97e
Song DongHui <14729626293@163.com>
2018-07-30 15:15:09 +0800

    add tow newspaper

--- jiSuanQi.js
+++ jiSuanQi.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
     var y01 = $("#y1").val();
     var y02 = $("#y2").val();
@@ -9,43 +7,40 @@ $(document).ready(function(){
     var y06 = $("#y6").val();
     var y07 = $("#y7").val();
     var y08 = $("#y8").val();
-    var title = {
-        text:'标准曲线'
-    };
-    var subtitle={
-        text: "fe"
-    };
-    var yAxis= {
+    data_list = [y01,y02,y03,y04,y05,y06,y07,y08];
+});
+alert(data_list); 
+var chart = Highcharts.chart("container",{
+
+    title:{
+        text: "标准曲线"
+    },
+    subtitle:{
+        text: "硫酸盐"
+    },
+    yAxis:{
         title:{
             text:"吸光度"
         }
-    };
-    var legend={
+    },
+    legend:{
         layout:"vertical",
-        align: "right",
-        verticalAlign: "middle"
-    };
-    var plotOptions={
+        align:"right",
+        verticalAlign:"middle"
+    },
+    plotOptions:{
         series:{
             label:{
-                connectorAllowed: false 
+                connectorAllowed:false
             },
             pointStart: 0
         }
-    };
-    var series=[{
-        name: "铁",
-        // val:[
-        //     0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9
-        // ],
-        // data:[
-        //     y01,y02,y03,y04,y05,y06,y07,y08
-        // ],
-        data:[
-            $("#y1").val(),$("#y2").val(),$("#y1").val(),$("#y2").val(),$("#y1").val(),$("#y2").val(),$("#y1").val(),$("#y2").val(),
-        ]
-    }];
-    var responsive={
+    },
+    series:[{
+        name: "Fe",
+        data: data_list
+    }],
+    responsive:{
         rules:[{
             condition:{
                 maxWidth: 500
@@ -53,20 +48,71 @@ $(document).ready(function(){
             chartOptions:{
                 legend:{
                     layout:"horizontal",
-                    align:"center",
-                    verticalAlign:"bottom"
+                    align: "center",
+                    verticalAlign: "bottom"
                 }
             }
         }]
-    };
-    var json={};
-    json.title = title;
-    json.subtitle = subtitle;
-    // json.xAxis = xAxis;
-    json.yAxis = yAxis;
-    json.legend = legend;
-    json.series = series;
-    json.responsive = responsive;
-    json.plotOptions = plotOptions;
-    $("#container").highcharts(json);
+    }
 });
+
+//    alert(data_list);
+//     var title = {
+//         text:'标准曲线'
+//     };
+//     var subtitle={
+//         text: "fe"
+//     };
+//     var yAxis= {
+//         title:{
+//             text:"吸光度"
+//         }
+//     };
+//     var legend={
+//         layout:"vertical",
+//         align: "right",
+//         verticalAlign: "middle"
+//     };
+//     var plotOptions={
+//         series:{
+//             label:{
+//                 connectorAllowed: false 
+//             },
+//             pointStart: 0
+//         }
+//     };
+//     var series=[{
+//         name: "铁",
+//         // val:[
+//         //     0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9
+//         // ],
+//         // data:[
+//         //     y01,y02,y03,y04,y05,y06,y07,y08
+//         // ],
+//         data: data_list
+//     }];
+//     var responsive={
+//         rules:[{
+//             condition:{
+//                 maxWidth: 500
+//             },
+//             chartOptions:{
+//                 legend:{
+//                     layout:"horizontal",
+//                     align:"center",
+//                     verticalAlign:"bottom"
+//                 }
+//             }
+//         }]
+//     };
+//     var json={};
+//     json.title = title;
+//     json.subtitle = subtitle;
+//     // json.xAxis = xAxis;
+//     json.yAxis = yAxis;
+//     json.legend = legend;
+//     json.series = series;
+//     json.responsive = responsive;
+//     json.plotOptions = plotOptions;
+//     $("#container").highcharts(json);
+// });
